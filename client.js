1f943fc25bacdc974dd687aa44f96d12a94c4293
Jared <jaredflomen@gmail.com>
2020-11-04 18:22:51 +0000

    Hard coded move up command

--- client.js
+++ client.js
@@ -5,8 +5,9 @@ const connect = function() {
     port: 50542
   });
   //Prints a message when the connection is successfully established
-  conn.on('connect', () => console.log("Connection successfully established to game server"))
-  conn.on('connect', () => conn.write("Name: JF"))
+  conn.on('connect', () => console.log("Connection successfully established to game server"));
+  conn.on('connect', () => conn.write("Name: JF"));
+  // conn.on('connect', data => conn.write("Move: up"));
 
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
