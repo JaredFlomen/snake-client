d99416afc6465f9b81afb2d0f6fe6e9742d935ab
Jared <jaredflomen@gmail.com>
2020-11-04 19:00:25 +0000

    Creating input file and exporting to play.js

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
 const connect = require('./client')
+const setupInput = require('./input');
 console.log('Connecting ...');
 connect();
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-}
-
-const handleUserInput = function (input) {
-  if (input === '\u0003') {
-    process.exit();
-  }
-}
-
 setupInput();
